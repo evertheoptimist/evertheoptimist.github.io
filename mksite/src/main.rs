445ca6468e2c3977745c7b317bdcdb0ab6c542cb
William Chargin <wchargin@gmail.com>
2022-03-27 19:23:53 -0700

    mksite: add simple frontmatter parsing

--- mksite/src/main.rs
+++ mksite/src/main.rs
@@ -1,3 +1,128 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+use std::path::Path;
+
+use yaml_rust::yaml::{Yaml, YamlLoader};
+
+mod frontmatter {
+    pub const DELIMITER: &str = "---";
+    pub const DATE: &str = "date";
+    pub const SLUG: &str = "slug";
+    pub const TITLE: &str = "title";
+    pub const WITHIN_DATE: &str = "withindate";
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct SortOrdinal {
+    date: String,
+    within_date: u32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Article {
+    slug: String,
+    title: String,
+    body: String,
+    sort_ordinal: SortOrdinal,
+}
+
+#[derive(Debug)]
+enum ArticleParseError {
+    MissingFrontmatterStart,
+    MissingFrontmatterEnd,
+    InvalidFrontmatterYamlSyntax(yaml_rust::scanner::ScanError),
+    FrontmatterNotHash,
+    MissingFrontmatterKey(&'static str),
+    InvalidFrontmatterValue(&'static str),
+    Io(io::Error),
+}
+impl From<io::Error> for ArticleParseError {
+    fn from(e: io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+
+fn parse_article_file(path: &Path) -> Result<Article, ArticleParseError> {
+    let mut f = BufReader::new(File::open(path)?);
+    let mut line = String::new();
+
+    // Find the frontmatter section, delimited by lines containing exactly "---".
+    if f.read_line(&mut line)? == 0 {
+        return Err(ArticleParseError::MissingFrontmatterStart);
+    }
+    if line.trim_end() != frontmatter::DELIMITER {
+        return Err(ArticleParseError::MissingFrontmatterStart);
+    }
+    let mut frontmatter = String::new();
+    loop {
+        line.clear();
+        if f.read_line(&mut line)? == 0 {
+            return Err(ArticleParseError::MissingFrontmatterEnd);
+        }
+        if line.trim_end() == frontmatter::DELIMITER {
+            break;
+        }
+        frontmatter.push_str(&line);
+    }
+
+    // Parse the frontmatter into a valid YAML hash.
+    let mut frontmatter = YamlLoader::load_from_str(&frontmatter)
+        .map_err(ArticleParseError::InvalidFrontmatterYamlSyntax)?;
+    if frontmatter.len() != 1 {
+        return Err(ArticleParseError::FrontmatterNotHash);
+    }
+    let frontmatter = match &mut frontmatter[0] {
+        Yaml::Hash(h) => h,
+        _ => return Err(ArticleParseError::FrontmatterNotHash),
+    };
+
+    // Extract keys of interest from the YAML frontmatter.
+    let mut get_key = |k: &'static str| {
+        frontmatter
+            .get_mut(&Yaml::String(String::from(k)))
+            .map(|x| std::mem::replace(x, Yaml::Null))
+            .ok_or(ArticleParseError::MissingFrontmatterKey(k))
+    };
+    let slug = get_key(frontmatter::SLUG)?.into_string().ok_or(
+        ArticleParseError::InvalidFrontmatterValue(frontmatter::SLUG),
+    )?;
+    let title = get_key(frontmatter::TITLE)?.into_string().ok_or(
+        ArticleParseError::InvalidFrontmatterValue(frontmatter::TITLE),
+    )?;
+    let sort_ordinal = SortOrdinal {
+        date: get_key(frontmatter::DATE)?.into_string().ok_or(
+            ArticleParseError::InvalidFrontmatterValue(frontmatter::DATE),
+        )?,
+        within_date: get_key(frontmatter::WITHIN_DATE)?
+            .into_i64()
+            .and_then(|n| u32::try_from(n).ok())
+            .ok_or(ArticleParseError::InvalidFrontmatterValue(
+                frontmatter::WITHIN_DATE,
+            ))?,
+    };
+
+    // Skip blank lines between frontmatter and article. (Probably not strictly necessary given
+    // that we'll parse the file as Markdown.)
+    let mut body = String::new();
+    loop {
+        f.read_line(&mut body)?;
+        if !body.trim().is_empty() {
+            break;
+        }
+        body.clear();
+    }
+    f.read_to_string(&mut body)?;
+
+    Ok(Article {
+        slug,
+        title,
+        sort_ordinal,
+        body,
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    drop(dbg!(parse_article_file(Path::new(
+        std::env::args_os().nth(1).expect("give arg").as_os_str()
+    ))));
 }
